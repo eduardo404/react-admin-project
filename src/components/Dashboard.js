4e764b803fcbbfb29f6c32e7396605e8a6a3189d
eoliveira <eduardoop31@gmail.com>
2021-01-21 09:47:47 -0300

    complete react admin tutorial

---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome to the administration" />
+        <CardContent>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia rerum vero tempore dolores explicabo officiis pariatur, aliquam ab cum accusamus eum quam iusto quos placeat nihil cumque esse est quas? Harum magni quibusdam quos ullam. Voluptas ducimus nulla fuga iure est quis labore, doloremque numquam vel eaque enim non facere soluta provident! Pariatur dolore reiciendis libero eius quas! Sint, facere?
+            </p>
+            <p>
+                Harum delectus voluptatem doloremque quae dolorem modi quaerat quibusdam ad ex deleniti pariatur mollitia, sapiente odit. Provident nam, vitae vel esse quos fugit corrupti doloribus quaerat corporis maiores, cum explicabo! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, rerum dolorem eaque unde doloremque at quis aut quam atque sapiente ipsum iusto molestiae porro! Qui nostrum beatae et adipisci cupiditate!
+            </p>
+        </CardContent>
+    </Card>
+);
